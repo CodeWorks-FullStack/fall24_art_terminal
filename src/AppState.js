c58b4713bec8a9fd098b5ca0c9ccf586461c07be
Jeremy Fowler <jembyfowler@gmail.com>
2024-09-19 11:45:41 -0600

    added profile property to appstate

--- src/AppState.js
+++ src/AppState.js
@@ -5,10 +5,14 @@ export const AppState = reactive({
   /**@type {import('@bcwdev/auth0provider-client').Identity} */
   identity: null,
   /** @type {import('./models/Account.js').Account} user info from the database*/
+  // NOTE for the logged in user
   account: null,
   /** @type {import('./models/Project.js').Project[]} */
   projects: [],
   /** @type {import('./models/Project.js').Project} */
-  activeProject: null
+  activeProject: null,
+  /** @type {import('./models/Account.js').Account} */
+  // NOTE for someone else's profile
+  activeProfile: null
 })
 
